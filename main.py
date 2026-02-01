031fa73bf2c456505cdb68f29a2f853d751d36a4
sagi-l <117835622+sagi-l@users.noreply.github.com>
2026-02-01 23:48:36 +0200

    Add webhook test v3 comment in main.py
    Added a comment for webhook test version 3.

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import redis
 
+#webhook test v3
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", "dev_secret")
 
